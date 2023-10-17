2a3cd0bd883a057b709ea4f3525d085c0c62facd
0dean <denusdednom5@gmail.com>
2023-10-17 23:55:55 +0300

    Added theme_provider and temporary themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:foodly/history.dart';
 import 'package:foodly/settings.dart';
+import 'package:foodly/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 import 'home.dart';
 import 'camera.dart';
 
 void main() async {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,9 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: Provider.of<ThemeProvider>(context).themeData,
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
--- lib/settings.dart
+++ lib/settings.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:foodly/theme/theme_provider.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class Settings extends StatefulWidget {
   const Settings({super.key});
@@ -18,8 +20,30 @@ class _SettingsState extends State<Settings> {
           style: GoogleFonts.roboto(fontSize: 25),
         ),
       ),
-      body: Container(
-
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primary,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          padding: const EdgeInsets.all(50),
+          child: GestureDetector(
+            onTap: () {
+              Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              padding: const EdgeInsets.all(25),
+              child: const Center(child: Text("TAP")),
+            ),
+          ),
+        ),
       ),
     );
   }
