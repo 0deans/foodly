5483d69cfe5a85fdd8bbc9b71b5bfbc4d51c8877
0dean <denusdednom5@gmail.com>
2023-10-22 23:17:15 +0300

    colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,28 +1,28 @@
 const List<int> categoryColors = [
   0xFF0d0d0d, // background
-  0xFFFF9800, // vegetables | leafy_greens (Amber)
+  0xFF912323, // vegetables | leafy_greens
   0xFF1b8d1b, // vegetables | stem_vegetables
-  0xFF795548, // vegetables | non-starchy_roots (Brown)
+  0xFF828205, // vegetables | non-starchy_roots
   0xFF272793, // vegetables | other
-  0xFFFF5722, // fruits (Deep Orange)
-  0xFF673AB7, // protein | meat (Deep Purple)
-  0xFF2196F3, // protein | poultry (Blue)
-  0xFF009688, // protein | seafood (Teal)
-  0xFFc51414, // protein | eggs (Amber)
+  0xFF828205, // fruits
+  0xFF128989, // protein | meat
+  0xFF828282, // protein | poultry
+  0xFF4c1010, // protein | seafood
+  0xFFc61717, // protein | eggs
   0xFF508b16, // protein | beans/nuts
-  0xFF795548, // starches/grains | baked_goods (Brown)
+  0xFFc3870f, // starches/grains | baked_goods
   0xFF612c96, // starches/grains | rice/grains/cereals
   0xFFc92693, // starches/grains | noodles/pasta
-  0xFF009688, // starches/grains | starchy_vegetables (Teal)
-  0xFF9E9E9E, // starches/grains | other (Grey)
-  0xFF3F51B5, // soups/stews (Indigo)
+  0xFF4c8888, // starches/grains | starchy_vegetables
+  0xFFc38686, // starches/grains | other
+  0xFF0f4b0f, // soups/stews
   0xFF915922, // herbs/spices
   0xFF2dcb2d, // dairy
-  0xFF607D8B, // snacks (Blue Grey)
-  0xFF9C27B0, // sweets/desserts (Purple)
-  0xFF03A9F4, // beverages (Light Blue)
+  0xFF91c823, // snacks
+  0xFF275d93, // sweets/desserts
+  0xFF8f578f, // beverages
   0xFF1cc78e, // fats/oils/sauces
   0xFF8dc68d, // food_containers
   0xFF585820, // dining_tools
-  0xFFcb612c, // other_food
+  0xFFcd6937, // other_food
 ];
