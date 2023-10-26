4f478acf738428aa7f96874ab2a81d48017f4b73
regmysss <kabanigor3@gmail.com>
2023-10-26 20:54:28 +0300

    something

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_uk.dart
@@ -0,0 +1,18 @@
+import 'app_localizations.dart';
+
+/// The translations for Ukrainian (`uk`).
+class AppLocalizationsUk extends AppLocalizations {
+  AppLocalizationsUk([String locale = 'uk']) : super(locale);
+
+  @override
+  String get camera => 'Камера';
+
+  @override
+  String get gallery => 'Галерея';
+
+  @override
+  String get history => 'Історія';
+
+  @override
+  String get settings => 'Налаштування';
+}
