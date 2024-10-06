2d50c1c2c2f91e48e29b4f3ee2a5176a2ae24620
Denys <denusdednom5@gmail.com>
2024-10-06 20:57:53 +0300

    refactor: Add support for capturing images on Android 12

--- lib/pages/camera.dart
+++ lib/pages/camera.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:foodly/widgets/icon_circle_button.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'accept_image.dart';
+import 'package:image/image.dart' as img;
+import 'package:foodly/utils/image.dart';
 
 class Camera extends StatefulWidget {
   const Camera({super.key});
@@ -65,13 +70,19 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
   }
 
   Future<void> _takePhoto() async {
-    if (!_controller.value.isInitialized) {
-      return;
+    if (!_controller.value.isInitialized) return;
+
+    XFile? image;
+
+    if (Platform.isAndroid) {
+      image = await _takePictureAndroid12();
+    } else {
+      image = await _controller.takePicture();
     }
 
-    _controller.takePicture().then((image) {
+    var xFile = image;
+    if (xFile != null) {
       _controller.pausePreview();
-
       setState(() {
         _controller.setFlashMode(FlashMode.off);
         _isFlash = false;
@@ -80,10 +91,63 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => AcceptImage(selectedImage: image),
+          builder: (context) => AcceptImage(selectedImage: xFile),
         ),
       ).then((_) => _controller.resumePreview());
+    }
+  }
+
+  Future<XFile?> _takePictureAndroid12() async {
+    // Ensure the image stream is not already running
+    if (_controller.value.isStreamingImages == true) {
+      await _controller.stopImageStream();
+    }
+
+    // Create a completer to wait for the image processing to complete
+    final completer = Completer<img.Image?>();
+
+    await _controller.startImageStream((cameraImage) async {
+      // Stop the image stream after receiving the image
+      await _controller.stopImageStream();
+
+      // Process the image
+      final img.Image image = switch (cameraImage.format.group) {
+        ImageFormatGroup.nv21 => nv21ToRgb(cameraImage), // custom function
+        ImageFormatGroup.jpeg => img.decodeImage(cameraImage.planes[0].bytes)!,
+        _ => yuv420ToRgb(cameraImage), // custom function
+      };
+
+      // Rotate the image based on sensor orientation
+      final img.Image rotated = img.copyRotate(
+        image,
+        angle: _controller.description.sensorOrientation,
+      );
+
+      print('Image captured ${rotated}');
+
+      // Complete the completer with the rotated image
+      completer.complete(rotated);
     });
+
+    // Wait for the image processing to finish
+    final rotatedImg = await completer.future;
+
+    if (rotatedImg == null) {
+      print('Error: Image not captured');
+      return null;
+    }
+
+    // Save the image data to a temporary file
+    final tempDir = await getTemporaryDirectory();
+    final filePath =
+        '${tempDir.path}/image_${DateTime.now().millisecondsSinceEpoch}.png';
+
+    // Write the bytes to the file
+    final file = File(filePath);
+    await file.writeAsBytes(img.encodePng(rotatedImg));
+
+    // Return the XFile created from the file
+    return XFile(filePath);
   }
 
   @override
