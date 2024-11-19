18e7a8b67fb59ccd5036a111945fbbe149de578b
regmysss <kabanigor3@gmail.com>
2024-11-19 09:39:51 +0200

    feat: Add launch configuration for VSCode and update GoogleSignIn service for getting idToken

--- mobile/lib/services/google_service.dart
+++ mobile/lib/services/google_service.dart
@@ -1,14 +1,24 @@
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleService {
-  static final _googleSignIn = GoogleSignIn();
+  static final _googleSignIn = GoogleSignIn(
+    // serverClientId: "your_server_client_id",
+  );
 
   static Future<GoogleSignInAccount?> signIn() async {
     try {
       final account = await _googleSignIn.signIn();
 
-      print(account);
-      return account;
+      if (account != null) {
+        final authentication = await account.authentication;
+
+        print("Google ID Token: ${authentication.idToken}");
+        print(account);
+
+        return account;
+      }
+
+      return null;
     } catch (e) {
       print(e);
     }
